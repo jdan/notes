27c405de5fcbc3447d9696084ddf0c1bf734673b
Jordan Scales <scalesjordan@gmail.com>
2022-05-14 16:35:21 -0400

    fix hashart client-side imports

--- index.js
+++ index.js
@@ -513,7 +513,7 @@ async function downloadImageBlock(block, blockId) {
 async function getHashArtHtml(block, piece, seed) {
   const pieceJs = await new Promise((resolve, reject) => {
     const b = browserify();
-    b.require(path.join(__dirname, `./node_modules/hashart/art/${piece}.js`))
+    b.require(path.join(__dirname, `./node_modules/hashart/art/${piece}.js`));
     b.bundle((err, js) => {
       if (err) {
         reject(err);
@@ -545,7 +545,10 @@ async function getHashArtHtml(block, piece, seed) {
     <script>${pieceJs}</script>
     <script>
       (() => {
-        const e = require("hashart/art/${piece}.js")
+        const e = require("${path.join(
+          __dirname,
+          `./node_modules/hashart/art/${piece}.js`
+        )}")
         // HACK: Each piece exports an object with a single key
         const art = new e[Object.keys(e)[0]]();
 
